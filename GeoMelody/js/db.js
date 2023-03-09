b9c490e5bb71321ebc32bccaa4974d10ebcf8649
Christopher Cussen <christopher.cussen2@mail.dcu.ie>
2023-03-09 12:56:27 +0000

    Delete db.js

--- GeoMelody/js/db.js
+++
@@ -1,9 +0,0 @@
-//real time listener
-
-//db.collection('songs').onSnapshot((snapshot) => {
-   // console.log(snapshot.docChanges());
-//})
-
-const unsub = onSnapshot(collection(db, "songs"), (collection) => {
-   console.log("Current data: ", collection.data());
-});
