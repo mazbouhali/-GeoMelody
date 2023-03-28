a3b9c5aa1fb62436a440606739722bea32171d67
cussenc2 <christopher.cussen2@mail.dcu.ie>
2023-03-28 18:48:01 +0100

    committing modified files index.js, about.html, history.html and signIn.html

--- GeoMelody/js/index.js
+++ GeoMelody/js/index.js
@@ -1,8 +1,9 @@
+
 import { initializeApp } from 'firebase/app'
 import {getFirestore, collection, getDocs, addDoc, doc, deleteDoc, onSnapshot, query, where, orderBy, serverTimestamp, getDoc, updateDoc } from 'firebase/firestore'
-import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from 'firebase/auth'
-import { getDatabase, ref, onValue } from "firebase/database";
+import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth'
 
+//Allows us to connect firebase to our project
 const firebaseConfig = {
     apiKey: "AIzaSyCJkcepY6HJgxynvSewRpW0oXkalE9nFH0",
     authDomain: "sample-b60fb.firebaseapp.com",
@@ -13,20 +14,20 @@ const firebaseConfig = {
   };
 
 initializeApp(firebaseConfig)
+
+// Authentication services
 const auth = getAuth()
 
 // init services
 const db = getFirestore()
 
-// collection ref
+// collection reference
 const colRef = collection(db, 'songs')
 
 // queries
 const q = query(colRef, orderBy('createdAt'))
 
-//database
-const dataBase = getDatabase()
-
+// get real time collection data
 onSnapshot(colRef, (snapshot) => {
     let songs = []
     snapshot.docs.forEach((doc) => {
@@ -35,55 +36,33 @@ onSnapshot(colRef, (snapshot) => {
     console.log(songs)
 })
 
-//adding songs
+/*
+//adding docs
 const addSongForm = document.querySelector('.add')
 addSongForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    
-    addDoc(colRef, {
-        artist: addSongForm.artist.value,
-        title: addSongForm.title.value,
-        createdAt: serverTimestamp()
-    })
-    .then(() => {
-        addSongForm.reset()
-    })
+  e.preventDefault()
 
+  addDoc(colRef, {
+    Title: addSongForm.title.value,
+    Artist: addSongForm.artist.value,
+    createdAt: serverTimestamp()
+  })
 })
 
-//deleting songs
+//deleting docs
 const deleteSongForm = document.querySelector('.delete')
 deleteSongForm.addEventListener('submit', (e) => {
-    e.preventDefault()
+  e.preventDefault()
 
-    const docRef = doc(db, 'songs', deleteSongForm.id.value)
-    deleteDoc(docRef)
-       .then(() => {
-          deleteSongForm.reset()
-       })
+  const docRef = doc(db, 'songs', deleteSongForm.id.value)
 
+  deleteDoc(docRef)
+  .then(() => {
+    deleteSongForm.reset()
+  })
 })
+*/
 
-// get a single doc
-const docRef = doc(db, 'songs', 'izsHhIoYfdMohCIWBfbA')
-
-onSnapshot(docRef, (doc) => {
-    console.log(doc.data(), doc.id)
-})
-
-//Update a document
-const updateForm = document.querySelector('.update')
-updateForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const docRef = doc(db, 'songs', updateForm.id.value)
-    updateDoc(docRef, {
-        title: 'updated title'
-    })
-    .then(() => {
-        updateForm.reset()
-    })
-})
 
 //signing a user up
 const signupForm = document.querySelector('.signup')
@@ -94,16 +73,17 @@ signupForm.addEventListener('submit', (e) => {
     const password = signupForm.password.value
 
     createUserWithEmailAndPassword(auth, email, password)
-       .then((cred) => {
-        console.log("user created:", cred.user)
-        signupForm.reset()
+        .then((cred) => {
+            console.log("user created:", cred.user)
+            signupForm.reset()
        })
        .catch((err) => {
-        console.log(err.message)
+            console.log(err.message)
        })
 })
 
-const logoutButton = document.querySelector('.login')
+
+const logoutButton = document.querySelector('.logout')
 logoutButton.addEventListener('click', () => {
     signOut(auth)
        .then(() => {
@@ -123,9 +103,69 @@ loginForm.addEventListener('submit', (e) => {
 
     signInWithEmailAndPassword(auth, email, password)
        .then((cred) => {
-        console.log('user logged in', cred.user)
+        console.log('user logged in:', cred.user)
        })
        .catch((err) => {
         console.log(err.message)
        })
 })
+
+onAuthStateChanged(auth, (user) => {
+    console.log("user status changed: ", user)
+})
+
+
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://shazam.p.rapidapi.com/shazam-events/list', //Shazam API Endpoint
+  //Parameters needed to retrieve the list of Shazam events
+  params: {artistId: '73406786', l: 'en-US', from: '2022-12-31', limit: '50', offset: '0'},
+  headers: {
+    'X-RapidAPI-Key': '745ce762a9msh0536864325395c8p1d4713jsne89175c7ad35',
+    'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+  }
+};
+
+/*
+//Sends the GET request to the API endpoint with the 'options' object as a parameter
+axios.request(options).then(function (response) {
+  //The response data from the Shazam API is logged to the console
+	console.log(response.data);
+}).catch(function (error) { //Error handling
+	console.error(error);
+});
+
+const form = document.getElementById('musicRecognitionForm');
+const input = document.getElementById('audioFile');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting
+
+  const file = input.files[0];
+  const reader = new FileReader();
+
+  reader.onloadend = function() {
+    const data = new FormData();
+    data.append('file', file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://shazam.p.rapidapi.com/shazam-events/list', true);
+    xhr.setRequestHeader('x-rapidapi-key', '745ce762a9msh0536864325395c8p1d4713jsne89175c7ad3');
+    xhr.setRequestHeader('x-rapidapi-host', 'shazam.p.rapidapi.com');
+
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        console.log(response);
+      }
+    }
+
+    xhr.send(data);
+  };
+
+  reader.readAsDataURL(file);
+  
+});
+*/
